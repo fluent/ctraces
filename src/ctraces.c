0d3de5570feb4f34bb323d6a22b502d24fc84177
Eduardo Silva <eduardo@calyptia.com>
2022-08-15 18:37:32 -0600

    core: destroy spans on exit
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- src/ctraces.c
+++ src/ctraces.c
@@ -67,8 +67,17 @@ struct ctrace *ctr_create(struct ctrace_opts *opts)
 
 void ctr_destroy(struct ctrace *ctx)
 {
+    struct cfl_list *head;
+    struct cfl_list *tmp;
+    struct ctrace_span *span;
+
     if (ctx->trace_id) {
         cfl_sds_destroy(ctx->trace_id);
+    }
+
+    cfl_list_foreach_safe(head, tmp, &ctx->spans) {
+        span = cfl_list_entry(head, struct ctrace_span, _head);
+        ctr_span_destroy(span);
     }
 
     free(ctx);
