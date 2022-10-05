fe5534ccc56356c57286a326bfd78274b6f51c53
Eduardo Silva <eduardo@calyptia.com>
2022-10-05 14:56:59 -0600

    random: add windows headers for random function
    Signed-off-by: Eduardo Silva <eduardo@calyptia.com>

--- src/ctr_random.c
+++ src/ctr_random.c
@@ -31,6 +31,14 @@
 #ifdef ITS_A_UNIX_FRIEND
 #include <fcntl.h>
 #include <unistd.h>
+#else
+// #define needed to link in RtlGenRandom(), a.k.a. SystemFunction036.  See the
+// "Community Additions" comment on MSDN here:
+// http://msdn.microsoft.com/en-us/library/windows/desktop/aa387694.aspx
+#define SystemFunction036 NTAPI SystemFunction036
+#include <ntsecapi.h>
+#undef SystemFunction036
+
 #endif
 
 #include <time.h>
