9e4454ea4ec3e4838717a5792aff41d25cda5005
Eduardo Silva <eduardo@chronosphere.io>
2026-07-12 17:34:22 -0600

    span: repair event integers and text formatting
    Signed-off-by: Eduardo Silva <eduardo@chronosphere.io>

--- tests/span.c
+++ tests/span.c
@@ -120,9 +120,68 @@ void test_random_id_length()
     ctr_id_destroy(id);
 }
 
+void test_event_integer_api()
+{
+    struct ctrace *ctx;
+    struct ctrace_resource_span *rs;
+    struct ctrace_scope_span *ss;
+    struct ctrace_span *span;
+    struct ctrace_span_event *event;
+    struct cfl_variant *value;
+
+    ctx = ctr_create(NULL);
+    rs = ctr_resource_span_create(ctx);
+    ss = ctr_scope_span_create(rs);
+    span = ctr_span_create(ctx, ss, "event", NULL);
+    event = ctr_span_event_add(span, "integer");
+    TEST_ASSERT(event != NULL);
+
+    TEST_CHECK(ctr_span_event_set_attribute_int(event, "int", 42) == 0);
+    TEST_CHECK(ctr_span_event_set_attribute_int64(event, "int64", INT64_MAX) == 0);
+    value = cfl_kvlist_fetch(event->attr->kv, "int64");
+    TEST_ASSERT(value != NULL);
+    TEST_CHECK(value->data.as_int64 == INT64_MAX);
+
+    ctr_destroy(ctx);
+}
+
+void test_text_encoder_optional_and_long_strings()
+{
+    char long_value[2048];
+    cfl_sds_t text;
+    struct ctrace *ctx;
+    struct ctrace_resource_span *rs;
+    struct ctrace_scope_span *ss;
+    struct ctrace_instrumentation_scope *scope;
+    struct ctrace_span *span;
+    struct ctrace_link *link;
+
+    memset(long_value, 'x', sizeof(long_value) - 1);
+    long_value[sizeof(long_value) - 1] = '\0';
+
+    ctx = ctr_create(NULL);
+    rs = ctr_resource_span_create(ctx);
+    ss = ctr_scope_span_create(rs);
+    scope = ctr_instrumentation_scope_create(NULL, NULL, 0, NULL);
+    ctr_scope_span_set_instrumentation_scope(ss, scope);
+    span = ctr_span_create(ctx, ss, "text", NULL);
+    link = ctr_link_create(span, NULL, 0, NULL, 0);
+    TEST_ASSERT(link != NULL);
+    TEST_CHECK(ctr_span_set_attribute_string(span, "long", long_value) == 0);
+
+    text = ctr_encode_text_create(ctx);
+    TEST_ASSERT(text != NULL);
+    TEST_CHECK(strstr(text, long_value) != NULL);
+
+    ctr_encode_text_destroy(text);
+    ctr_destroy(ctx);
+}
+
 TEST_LIST = {
     {"span", test_span},
     {"resource_span_direct_destroy", test_resource_span_direct_destroy},
     {"random_id_length", test_random_id_length},
+    {"event_integer_api", test_event_integer_api},
+    {"text_encoder_optional_and_long_strings", test_text_encoder_optional_and_long_strings},
     { 0 }
 };
